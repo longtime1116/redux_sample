639dc3822a5aff0656f1758de57e52a55a5cdf9b
shumo.tokinaga <longtime1116@gmail.com>
2018-01-09 15:25:42 +0900

    change hello to app

---
+++ src/components/App.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+export class App extends React.Component {
+  render(){
+    return <h1>App</h1>
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDom from "react-dom"
-import {Hello} from "./Hello"
+import {App} from "./components/App"
 
 ReactDom.render(
-  <Hello />,
+  <App />,
   document.getElementById("root")
 )
